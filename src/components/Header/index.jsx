768e74e71b2ff0ea3731fb0c62d2fc18fbc50418
Linh <lixxkook@Macbut-M1-cua-Linh.local>
2022-12-14 15:26:36 +0700

    complete connect wallet

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,24 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.scss'
 
-import { connectWallet } from "../../utils/connectWallet"
+import { connectWallet, disconnectWallet } from "../../utils/connectWallet"
 
 export default function Header() {
-  return (
-    <div className="header">
-        <div className="container">
-            <div className="header-wrapper d-flex justify-content-between align-items-center">
-                <div className="header-logo">
-                    <img className="img-fluid" src="./images/logo.png" alt="" />
-                </div>
 
-                <div className="button btn btn-primary"
-                    onClick={() => connectWallet()}
-                >
-                    Connect Wallet
+    const [address, setAddress] = useState(window.ethereum !== undefined ? window.ethereum.selectedAddress : "")
+
+    const handleConnectWallet = async () => {
+        const addressConnected = await connectWallet();
+        setAddress(addressConnected);
+    }
+
+    return (
+        <div className="header">
+            <div className="container">
+                <div className="header-wrapper d-flex justify-content-between align-items-center">
+                    <div className="header-logo">
+                        <img className="img-fluid" src="./images/logo.png" alt="" />
+                    </div>
+
+                    <div className="button btn btn-primary"
+                        onClick={() => handleConnectWallet()}
+                    >
+                        { address === "" ? "Connect Wallet" : address}
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
--- src/utils/connectWallet.js
+++ src/utils/connectWallet.js
@@ -1,11 +1,32 @@
-export function connectWallet() {
+export async function connectWallet() {
     if(window.ethereum){
-        window.ethereum.request({method:'eth_requestAccounts'})
-        .then(res=>{
-            console.log(res) 
-            console.log(window.ethereum)
-        })
+        try {
+            await window.ethereum.request({ method: 'eth_requestAccounts' })
+            if (window.ethereum.selectedAddress) {
+                return window.ethereum.selectedAddress;
+            }
+        } catch (error) {
+            return error
+        }
+    }else{
+      alert("install metamask extension!!")
+    }
+}
 
+export async function disconnectWallet() {
+    if(window.ethereum){
+        try {
+            await window.ethereum.request({
+                method: "wallet_requestPermissions",
+                params: [
+                {
+                    eth_accounts: {}
+                }
+                ]
+            });
+        } catch (error) {
+            return error
+        }
     }else{
       alert("install metamask extension!!")
     }
